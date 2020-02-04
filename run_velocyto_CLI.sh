47daeb752e2e3d77bc8e84678bbcbafc9d972e04
Rick Li <31550137+rzzli@users.noreply.github.com>
2020-02-03 16:09:31 -0800

    Create run_velocyto_CLI.sh

---
+++ run_velocyto_CLI.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+velocyto run10x -m /data/scratch/ouyang/sc10X/refdata-cellranger-GRCh38-1.2.0/genes/hg38_rmsk.gtf /data/archive/19-10-15-ES/cellranger/ES_sample_Oct15 /data/scratch/ouyang/sc10X/refdata-cellranger-GRCh38-1.2.0/genes/genes.gtf
